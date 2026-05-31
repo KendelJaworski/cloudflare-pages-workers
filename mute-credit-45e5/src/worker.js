642ed44160de5e225ae26dcf53f4a9e29ce141b4
kendel jaworski <user@example.com>
2026-05-31 13:19:27 -0500

    testing a worker change 3

--- mute-credit-45e5/src/worker.js
+++ mute-credit-45e5/src/worker.js
@@ -12,6 +12,6 @@ export default {
   async fetch(request, env, ctx) {
     // You can view your logs in the Observability dashboard
     console.info({ message: 'Hello World Worker received a request!' }); 
-    return new Response('worker 2!');
+    return new Response('worker 2, lets hope only 1 of these deploy!');
   }
 };
